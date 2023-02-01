87630fc42d11151d19abc98ce24f16b9f8037c49
soongu <naong@naver.com>
2023-02-01 09:31:45 +0900

    # JAR 배포설정 2

--- src/main/java/com/example/todo/HealthCheckController.java
+++ src/main/java/com/example/todo/HealthCheckController.java
@@ -3,13 +3,11 @@ package com.example.todo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@CrossOrigin
 public class HealthCheckController {
 
     @GetMapping("/")
